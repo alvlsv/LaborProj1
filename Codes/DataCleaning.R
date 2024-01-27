2f8397f61344e3cb6531204dca165ca57ccdc99b
avlsv <156054935+alvlsv@users.noreply.github.com>
2024-01-27 20:15:08 +0300

    Updates

--- Codes/DataCleaning.R
+++ Codes/DataCleaning.R
@@ -3,12 +3,7 @@ library(locpol)
 library(haven)
 
 
-data <- read_dta("data/Round28WF.dta")
-
-
-data$`_v170`[data$`_v170` < 1e+05] |> na.omit() |> hist()
-
-data$xj81[data$xj81 < 1e+05] |> hist()
+data_full <- read_dta("data/Round28WF.dta")
 
 
 
@@ -22,27 +17,50 @@ data$xj81[data$xj81 < 1e+05] |> hist()
 #xj10+xj40	Сколько заплатили чистыми за последние 30 дней
 #xj32	Есть ли еще работа
 
-choice <- 
-  data |> 
-  select(region, 
-         xh5, 
-         xi1, 
-         `_v23`, 
-         `_v176`, 
-         `_v181`, 
-         `_v145`,
-         `_v181`, 
-         xj85, 
-         x_age) |> 
-  mutate(wage = `_v23`, 
-         education = `_v145`, 
-         experience = `_v176`,
-         has_wage = is.na(wage)*(wage == 0)*1,
-         gender = xh5,
-         unempl_wage = `_v181`,
-         unempl_reg = xj85, 
-         age = x_age, 
-         another_region = xi1)
+choice <-
+  data_full |>
+  select(
+    region,
+    xh5,
+    xi1,
+    `_v23`,
+    `_v176`,
+    `_v181`,
+    `_v145`,
+    `_v181`,
+    xj85,
+    x_age,
+    xi2,
+    xj56,
+    xm80,
+    xm3,
+    xj1,
+    xi3,
+    xj2cod08,
+    x_marst,
+    `_v54`,
+    xj90
+  ) |>
+  rename(
+    wage_0 = `_v23`,
+    education = `_v145`,
+    experience = `_v176`,
+    gender = xh5,
+    unempl_wage = `_v181`,
+    unempl_reg = xj85,
+    age = x_age,
+    relocated = xi1,
+    other_work = xj56,
+    drinking = xm80,
+    health = xm3,
+    born_in_city = xi3,
+    republic = xi2,
+    profession = xj2cod08,
+    marital_status = x_marst,
+    now_in_school=`_v54`,
+    line = xj90
+  ) |>  
+  mutate_at(vars(wage_0), ~replace(., is.na(.)| .>10e+07, 0))|> rename(wage=wage_0) |> filter(now_in_school!=1)
+
 
 write_csv(choice, "data/dataset.csv")
-choice$education
--- Codes/Estimator.R
+++ Codes/Estimator.R
@@ -3,27 +3,25 @@
 # Libraries ----
 library(tidyverse)
 library(locpol)
+library(sampleSelection)
 
-#datafull<-
+#Data ---- 
 data <- read_csv("data/dataset.csv")
-data_no_na <- data |> na.omit() |> filter(wage > 0)
-
 
 
 # Simple Mincer ----
 lm(
-  log(wage) ~ education + experience * education + I(experience ^ 2) * I(education ^ 2) + region +,
-  data_no_na
+  log(wage) ~ education + experience * education + I(experience ^ 2) * I(education ^ 2) , data |> filter(wage > 0, wage<1e+05)
 ) |> summary()
 
 
-
+data |> select(wage) 
+  
 # Correction for Selection  ---
 
 ## Participation model ----
-participation_model <-
-  glm(has_wage ~ region + xi1, data, family = binomial(link = "logit"))
 
+selection(has_wage~ education,  wage ~ experience+education, data)
 
 
 ## Local Model ----
