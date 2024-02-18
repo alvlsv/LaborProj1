3f7cdca099628256e8c4ba8daa4277d63b1ca99a
avlsv <156054935+alvlsv@users.noreply.github.com>
2024-02-18 18:43:33 +0300

    Update Code.R

--- Project 2/Code/Code.R
+++ Project 2/Code/Code.R
@@ -5,11 +5,32 @@ library(haven)
 
 dataset_raw <- read_dta("Project 2 data after stset.dta",encoding = "latin1")
 
-write.csv(dataset_raw, file="data_raw.csv")
-
-coxph
+write.csv(dataset_raw, file = "data_raw.csv")
 
 
+dataset_male <-
+  dataset_raw |>
+  filter(gender == 0) |>
+  select(no_rab, 
+         date_rab, 
+         god_roz, 
+         semejnoe, 
+         ivdiwen, 
+         agecat, 
+         otn_zan, 
+         otrasl_s) |>
+  na.omit() |>
+  rename(id = no_rab,
+         marital_status = semejnoe,
+         birth_year = god_roz,
+         age_cat = agecat,
+         n_dependents = ivdiwen, 
+         employment_status = otn_zan,
+         sector_of_last_emp = otrasl_s,
+         raion = 
+         )
+
+dataset_raw$raion |> hist()
 
 muhaz
 
